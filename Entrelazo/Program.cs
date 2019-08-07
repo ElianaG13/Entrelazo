3da6b1b306623404ae9ce22b39c6d20121ac2bf1
Eliana13 <elianagc22@hotmail.com>
2019-08-07 12:19:19 -0500

    Initial commit

---
+++ Entrelazo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Entrelazo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Proyecto Entrelazo= new Proyecto();
+
+            Entrelazo.IngresarProyecto();
+            Entrelazo.ImprimirProyecto();
+            Entrelazo.AumentoS();
+
+
+        }
+    }
+}
